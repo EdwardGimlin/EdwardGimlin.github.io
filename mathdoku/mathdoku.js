89e449be85edb9708460daa10b50c68c44adb06e
EdwardGimlin <123013097+EdwardGimlin@users.noreply.github.com>
2026-04-05 17:09:58 -0400

    Create mathdoku.js

---
+++ mathdoku/mathdoku.js
@@ -0,0 +1,112 @@
+// Create nav bar element:
+let navBar = document.createElement("p");
+navBar.id = "navBar";
+navBar.innerHTML = "Try them all: ";
+document.body.appendChild(navBar);
+
+//Store info for list of links
+let linkMap = new Map([
+  ["Link 1", "https://edwardgimlin.github.io/"],
+  ["Link 2", "https://edwardgimlin.github.io/"],
+  ["Link 3", "https://edwardgimlin.github.io/"]
+
+]);
+
+//Add links to nav bar
+linkMap.forEach (function(value, key) {
+  q = document.createElement("a");
+  q.innerText = key;
+  q.href = value;
+  document.getElementById("navBar").appendChild(q);
+  navBar.innerHTML += " | ";
+})
+
+//Add link back to main page
+let mainPageLink = document.createElement("a");
+mainPageLink.innerText = "Return to Main Page";
+mainPageLink.href = "https://edwardgimlin.github.io/";
+document.getElementById("navBar").appendChild(mainPageLink);
+
+//Toggle the rules display
+let rulesButton = document.createElement("button");
+rulesButton.innerHTML = "Show/Hide rules";
+rulesButton.addEventListener("click", showRules);
+document.body.appendChild(rulesButton);
+
+//Place to show the rules
+let rulesDisplay = document.createElement("p");
+rulesDisplay.id = "rulesDisplay";
+rulesDisplay.innerHTML = "";
+document.body.appendChild(rulesDisplay);
+
+let breaker = document.createElement("br");
+document.body.appendChild(breaker);
+
+//Create the grid
+// TODO: will probably have to move this to HTML. The borders will be different for each one
+for(let i = 1; i < 17; i++) {
+  q = document.createElement("input");
+  q.type = "text";
+  q.id = i.toString();
+  q.style.border = "1px solid";
+  document.body.appendChild(q);
+  if(i % 4 == 0) {
+    rowBreak = document.createElement("br");
+    document.body.appendChild(rowBreak);
+  }
+}
+
+//Create the buttons
+let checkButton = document.createElement("button");
+checkButton.innerHTML = "Check Answer";
+checkButton.addEventListener("click", checkAnswer);
+document.body.appendChild(checkButton);
+
+let resetButton = document.createElement("button");
+resetButton.innerHTML = "Reset";
+resetButton.addEventListener("click", createGrid);
+document.body.appendChild(resetButton);
+
+
+
+//Create the results display
+let scoreParagraph = document.createElement("p");
+scoreParagraph.id = "score";
+document.body.appendChild(scoreParagraph);
+
+let answerString;
+let startingString;
+
+//checks if solution is correct
+function checkAnswer() {
+  var x = "";
+  for(let i = 1; i < 16; i++) {
+    x += document.getElementById(i.toString()).value;
+  }
+  if(x == answerString) {
+    document.getElementById("score").innerHTML = "correct";
+  }
+  else {
+    document.getElementById("score").innerHTML = "incorrect";
+  }
+}
+
+//create or restart grid
+function createGrid() {
+  for(let i = 1; i < 16; i++) {
+    document.getElementById(i.toString()).value = "";
+    document.getElementById(i.toString()).maxLength = 1;
+    
+  }
+  
+}
+
+//Display the rules
+function showRules() {
+  if(document.getElementById("rulesDisplay").innerHTML == "") {
+    document.getElementById("rulesDisplay").innerHTML = "Fill in each square with a number from 1 to 9. The same number can only show up once in each row, column, and 9x9 box.";
+  }
+  else {
+    document.getElementById("rulesDisplay").innerHTML = "";
+  }
+}
